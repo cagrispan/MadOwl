2d51128758990dd115ecdcc5a24ba5944639ebcc
Johnatan L Borges <johnatanborges@hotmail.com>
2019-06-26 17:19:03 -0300

    Barra size with scroll

--- general.js
+++ general.js
@@ -2,13 +2,16 @@ window.onscroll = function() {
     let screen = window.innerHeight;
     let scroll = window.pageYOffset;
     if (scroll >= screen) {
-        document.getElementById('coruja').style.display = "block";
-        document.getElementById('header-mobile').style.visibility = "visible";
+        document.getElementById('coruja').style.display = 'block';
+        document.getElementById('header-mobile').style.visibility = 'visible';
+        document.getElementById('barra').style.top = '70px';
+        document.getElementById('nav').style.top = '0px';
 
     }
     else {
-        document.getElementById('coruja').style.display = "none";
-        document.getElementById('header-mobile').style.visibility = "hidden";
-
+        document.getElementById('coruja').style.display = 'none';
+        document.getElementById('header-mobile').style.visibility = 'hidden';
+        document.getElementById('barra').style.top = '0';
+        document.getElementById('nav').style.top = '70px';
     }
 };
