99f7531a7d4ff751525c36d2300ae53865b7251d
Johnatan L Borges <johnatanborges@hotmail.com>
2019-06-26 16:27:00 -0300

    Toolbar white hidden

--- general.js
+++ general.js
@@ -3,8 +3,12 @@ window.onscroll = function() {
     let scroll = window.pageYOffset;
     if (scroll >= screen) {
         document.getElementById('coruja').style.display = "block";
+        document.getElementById('header-mobile').style.visibility = "visible";
+
     }
     else {
         document.getElementById('coruja').style.display = "none";
+        document.getElementById('header-mobile').style.visibility = "hidden";
+
     }
 };
