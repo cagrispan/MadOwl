72fe13bfbf321ad3262edad56e9e1cc40595c6a7
Johnatan L Borges <johnatanborges@hotmail.com>
2019-06-27 18:58:51 -0300

    Toolbar black

--- general.js
+++ general.js
@@ -1,17 +1,30 @@
 window.onscroll = function() {
     let screen = window.innerHeight;
     let scroll = window.pageYOffset;
-    if (scroll >= screen) {
+    if (scroll > 0) {
         document.getElementById('coruja').style.display = 'block';
         document.getElementById('header-mobile').style.visibility = 'visible';
-        document.getElementById('barra').style.top = '70px';
-        document.getElementById('nav').style.top = '0px';
+        document.getElementById('coruja-pc').style.display = 'block';
+        document.getElementById('nav-pc').style.backgroundColor = 'white';
 
+        let x = document.getElementsByClassName("item");
+        let i;
+            for (i = 0; i < x.length; i++) {
+            x[i].style.color = "black";
+            }
     }
+
     else {
         document.getElementById('coruja').style.display = 'none';
         document.getElementById('header-mobile').style.visibility = 'hidden';
-        document.getElementById('barra').style.top = '0';
-        document.getElementById('nav').style.top = '70px';
+        document.getElementById('coruja-pc').style.display = 'none';
+        document.getElementById('nav-pc').style.backgroundColor = 'black';
+
+        let x = document.getElementsByClassName("item");
+        let i;
+            for (i = 0; i < x.length; i++) {
+            x[i].style.color = "white";
+            }
     }
+
 };
